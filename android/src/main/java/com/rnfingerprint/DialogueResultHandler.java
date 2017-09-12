cd343f47d9ff7233b7825a6b8f9737e667150113
Eli Zibin <eli@laterolabs.com>
2017-09-12 09:26:16 -0700

    Copy over fingerprint files for Android

---
+++ android/src/main/java/com/rnfingerprint/DialogueResultHandler.java
@@ -0,0 +1,28 @@
+package com.rnfingerprint;
+
+import com.facebook.react.bridge.Callback;
+import android.util.Log;
+
+public class DialogResultHandler implements FingerprintDialog.DialogResultListener {
+    private Callback errorCallback;
+    private Callback successCallback;
+    public DialogResultHandler(Callback reactErrorCallback, Callback reactSuccessCallback) {
+      errorCallback = reactErrorCallback;
+      successCallback = reactSuccessCallback;
+    };
+
+    @Override
+    public void onAuthenticated() {
+        Log.d("***DIALOG RESULT HANDLER ", "***On authenticated dialog handler called");
+        successCallback.invoke("Successfully authenticated.");
+    }
+    @Override
+    public void onError(String errorString) {
+        Log.d("***DIALOG RESULT ERROR ",errorString);
+        errorCallback.invoke(errorString);
+    }
+    @Override
+    public void onCancelled() {
+        errorCallback.invoke("cancelled");
+    }
+}
