9b0da3c67b988e4d227fa45dcd1efeacf2f92741
Eli Zibin <eli@laterolabs.com>
2017-09-12 10:11:37 -0700

    update image, rename files properly

--- TouchID.android.js
+++ TouchID.android.js
@@ -1,13 +1,51 @@
-/**
- * Stub of TouchID for Android.
- *
- * @providesModule TouchID
- * @flow
- */
-'use strict';
+import { NativeModules } from 'react-native';
+const NativeTouchID = NativeModules.FingerprintAuth;
 
 export default {
   isSupported() {
-    return false;
+    return new Promise((resolve, reject) => {
+      NativeTouchID.isSupported(
+        error => {
+          return reject(typeof error == 'String' ? createError(error, error) : createError(error));
+        },
+        success => {
+          return resolve(true);
+        }
+      );
+    });
+  },
+
+  authenticate(reason, config = null) {
+    var authReason;
+    if (reason) {
+      authReason = reason;
+    } else {
+      authReason = ' ';
+    }
+
+    return new Promise((resolve, reject) => {
+      NativeTouchID.authenticate(
+        authReason,
+        error => {
+          return reject(typeof error == 'String' ? createError(error, error) : createError(error));
+        },
+        success => {
+          return resolve(true);
+        }
+      );
+    });
   }
 };
+
+function TouchIDError(name, details) {
+  this.name = name || 'TouchIDError';
+  this.message = details.message || 'Touch ID Error';
+  this.details = details || {};
+}
+
+TouchIDError.prototype = Object.create(Error.prototype);
+TouchIDError.prototype.constructor = TouchIDError;
+
+function createError(error) {
+  return new TouchIDError('Touch ID Error', error);
+}
